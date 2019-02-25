d6443f65f301127afca2c4b7533baeedf50da5d2
jigneshm <jigneshm@wordsworthelt.in>
2019-02-25 15:04:36 +0530

    demo

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1><label>Name: </label>{{ name }}</h1>
+    <h4><label>Gender: </label>{{ gender }}</h4>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+     /* name: this.$store.getters.getName,
+      gender: this.$store.getters.getGender,*/
+    }
+  },
+  created() {
+   setTimeout(()=> {
+    this.$store.commit('setGender',"Female");
+    this.$store.commit('setName',"Sujata");
+   } ,2000 ) 
+  },
+  computed: {
+    ...mapGetters({
+      name:'getName',
+      gender:'getGender'
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		name:"Vipul",
+		gender:"Male"
+	},
+	getters: {
+	    getName: state => {
+	      return state.name;
+	    },
+	    getGender: state => {
+	      return state.gender;
+	    }
+	},
+	mutations: {
+		setName: (state,payload) => {
+			state.name = payload;
+		},
+		setGender: (state,payload) => {
+			state.gender = payload;
+		},
+	}
+});
+
+export default store;
